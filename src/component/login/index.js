2f96faabd76516d588c32c4469a261a91269ad6e
priyakarunakaran21 <priyadharshinik@emids.com>
2023-06-28 15:46:41 +0530

    Add files via upload

--- src/component/login/index.js
+++ src/component/login/index.js
@@ -8,15 +8,16 @@ const Login = ({isExist, onCredChange, handleKeyPress, onSubmit, error}) =>{
       }
    
     return(
-       
+       <div className="loginbody">
         <div className="login-form">
             <div className="centeredDiv">
         {!isExist && <span className="error">{error}</span>}
-        <h1>Enter your user name</h1>
+        <h1>Personal Assistant</h1>
         <div className="input-container">
         <input type="text" name="user" onChange={handleDataChange} onKeyDown={handleKeyPress} placeholder="Username"/>
         <input type="password" name="password" placeholder="Password" onChange={handleDataChange} onKeyDown={handleKeyPress}/>
         <button type="submit" onClick={onSubmit}>Submit</button>
+        </div>
         </div>
         </div>
         </div>
